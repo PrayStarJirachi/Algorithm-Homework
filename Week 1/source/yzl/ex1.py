e0542e13798bb2fc370f7956adbf200e5b2d4545
Jirachi <PrayStarJirachi@gmail.com>
2016-10-12 21:41:53 +0800

    Modified.

--- Week 1/source/yzl/ex1.py
+++ Week 1/source/yzl/ex1.py
@@ -1,6 +1,8 @@
-def C(n, k)
-    if (n == 0 && k == 0) return 1;
-    if (n < k) return 0;
-    if (k < 0) return 0;
+def C(n, k) :
+    if (n == 0 and k == 0) :
+    	return 1;
+    if (n < k) :
+    	return 0;
+    if (k < 0) :
+    	return 0;
     return C(n - 1, k) + C(n - 1, k - 1);
-    
